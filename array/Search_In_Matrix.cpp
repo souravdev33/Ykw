d4317c65e19ffea459e8256ff48ae2f4d8139951
Sourav Dev <130246603+souravdev33@users.noreply.github.com>
2024-11-17 15:50:26 +0600

    committed

---
+++ array/Search_In_Matrix.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <algorithm>
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define debug(x) cerr << #x << " = " << (x) << endl;
+using ll= long long;
+using namespace std;
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n,m;
+    cin>>n>>m;
+
+    vector<vector<int>>arr(n, vector<int>(m));
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+        }
+    }
+    int x;
+    cin>>x;
+    bool flag=false;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(arr[i][j]==x){
+                flag=true;
+                break;
+            }
+        }
+    }
+
+    if(flag){
+        cout<<"will not take number"<<endl;
+    }
+    else{
+        cout<<"will take number"<<endl;
+    }
+    return 0;
+}
