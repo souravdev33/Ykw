559e958b3299451848f538210a0b45f1a23a86d7
Sourav Dev <130246603+souravdev33@users.noreply.github.com>
2024-11-19 02:04:01 +0600

    committed

--- Bitwise and bitmasks/Playing_with_bits.cpp
+++ Bitwise and bitmasks/Playing_with_bits.cpp
@@ -7,19 +7,29 @@
 using ll= long long;
 using namespace std;
 
+
 int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n;
-    cin>>n;
+    int a, b;
+    cin >> a >> b;
 
-    int count=0;
-    while(n>0){
-        count+=(n&1);
-        n=n>>1;
+    int sum = 0;
+
+    for (int i = a; i <= b; i++) {
+        int num = i;
+        int count = 0; 
+
+        while (num > 0) {
+            count += (num & 1); 
+            num = num >> 1;     
+        }
+
+        sum += count; 
     }
-    cout<<count<<endl;
+
+    cout << sum << endl;
+
     return 0;
 }
-
